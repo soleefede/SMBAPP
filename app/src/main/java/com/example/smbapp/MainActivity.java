9942eeb49a0c7028418b5796ea55124a549e6b76
emily <0918277686>
2019-07-27 16:06:05 +0800

    Add map feature.

--- app/src/main/java/com/example/smbapp/MainActivity.java
+++ app/src/main/java/com/example/smbapp/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.smbapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.supportMapFragment);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng targetLocation = new LatLng(24.826333, 121.770532);
+
+        int height = getResources().getDimensionPixelSize(R.dimen.icon_size);
+        int width = getResources().getDimensionPixelSize(R.dimen.icon_size);
+        BitmapDrawable bitmapdrawable=(BitmapDrawable)getResources().getDrawable(R.drawable.icon);
+        Bitmap smallMarker = Bitmap.createScaledBitmap(bitmapdrawable.getBitmap(), width, height, false);
+        mMap.addMarker(new MarkerOptions().position(targetLocation).title("HoYa Rice Kitchen").icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(targetLocation));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(16f));
+
     }
 }
